3b6350b811d3c403133d90e279c41c511583d473
Sardor <sardor88.88@mail.ru>
2019-07-11 11:02:35 +0500

    sardor

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -22,3 +22,9 @@ $this->registerJs('
 ');
 
 ?>
+<script>
+window.replainSettings = { id: '0a09140a-6481-4f7e-9ff9-7ddb25d1d079' };
+(function(u){var s=document.createElement('script');s.type='text/javascript';s.async=true;s.src=u;
+var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);
+})('https://widget.replain.cc/dist/client.js');
+</script>
